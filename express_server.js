e1362c1ed42a9d305018c5c855a66c4709783991
Jungmin Cha <ckwjdals90@gmail.com>
2016-11-11 05:25:20 +0000

    registration and authentication are completed and tested

--- express_server.js
+++ express_server.js
@@ -2,12 +2,23 @@
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cookieParser())
+app.use(cookieParser());
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(cookieSession({
+  name: 'session',
+  keys: ['secretkey1', 'secretkey2']
+}))
 
 app.set("view engine", "ejs");
 
@@ -23,14 +34,48 @@ app.get("/", (req, res) => {
 });
 
 // //login and logout
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
 app.post("/login", (req, res) => {
-  res.cookie("useremail", req.body.useremail);
-  res.redirect("/urls");
+  const emailInput = req.body.useremail;
+  const passwordInput = req.body.password;
+
+  var emailFound = "";
+  var passwordFound = "";
+  Object.keys(users).forEach((userId) => {
+    if (users[userId].email === emailInput) {
+      emailFound = users[userId].email;
+      passwordFound = users[userId].password;
+    }
+  });
+
+  if (emailInput === emailFound) {
+    console.log("email found in the db");
+    // var passwordMatch;
+    bcrypt.compare(passwordInput, passwordFound, (err, passwordMatch) => {
+      if (passwordMatch) {
+      console.log("password matches, good to go");
+      req.session.useremail = req.body.useremail;
+      res.redirect("/urls");
+      return;
+    } else {
+      console.log("wrong password");
+      // res.send("Invalid email or password");
+      res.status(403).send("Invalid email or password");
+      return;
+    }
+  });
+  } else {
+    console.log("email not found");
+    res.status(403).send("Invalid email or password");
+    return;
+  }
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("useremail");
+  req.session.useremail = undefined;
   res.redirect("/urls");
 });
 
@@ -40,21 +85,37 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
+  var unavailableEmail = "";
+  Object.keys(users).forEach((userId) => {
+    if (users[userId].email === req.body.email) {
+      console.log(users[userId].email);
+      unavailableEmail = users[userId].email;
+    }
+  });
 
   if (req.body.email === "" || req.body.password === "") {
     res.status(400).send("Please enter your email and password!");
-  } else if (users[req.body.email]){
+  } else if (unavailableEmail === req.body.email){
     res.status(400).send("email unavailable");
   } else {
+    let enteredEmail = req.body.email;
+    let enteredPassword = req.body.password;
     let uniqueID = generateRandomString()
-    users[uniqueID] = req.body;
-    console.log(users);
+    bcrypt.hash(enteredPassword, saltRounds, (err, hash) => {
+      const newUser = { email: enteredEmail, password: hash };
+      console.log(hash);
+      console.log(newUser);
+      users[uniqueID] = newUser;
+      console.log(users);
+    });
+    // users[uniqueID] = { email: enteredEmail, password: hashPassword };
+
     res.redirect("/urls");
   }
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { useremail: req.cookies.useremail, urls: urlDatabase };
+  let templateVars = { useremail: req.session.useremail, urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
