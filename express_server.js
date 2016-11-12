7cf58d7df6adb1596a48e314da66b8d9c2a67434
Jungmin Cha <ckwjdals90@gmail.com>
2016-11-12 03:20:17 +0000

    Prototype complete.

--- express_server.js
+++ express_server.js
@@ -4,13 +4,14 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+
 const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
 const someOtherPlaintextPassword = 'not_bacon';
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 app.set('trust proxy', 1) // trust first proxy
@@ -23,8 +24,6 @@ app.use((req, res, next) => {
   res.locals.urls = urlDatabase;
   next();
 });
-
-// const templateVars = { useremail: req.session.useremail, urls: urlDatabase };
 
 app.set("view engine", "ejs");
 
@@ -39,7 +38,6 @@ var users = {
   };
 
 app.get("/", (req, res) => {
-
   res.render("homepage");
 });
 
@@ -119,15 +117,12 @@ app.post("/register", (req, res) => {
       users[uniqueID] = newUser;
       console.log(users);
     });
-    // users[uniqueID] = { email: enteredEmail, password: hashPassword };
 
     res.redirect("/urls");
   }
 });
 
 app.get("/urls", (req, res) => {
-// const templateVars = { useremail: req.session.useremail, urls: urlDatabase };
-
   res.render("urls_index");
 });
 
@@ -142,13 +137,11 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-// const templateVars = { useremail: req.session.useremail, urls: urlDatabase };
   res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { para: req.params.id }
-// templateVars = { useremail: req.session.useremail, urls: urlDatabase, para: req.body.id }
 
   res.render("urls_show", templateVars);
 });
@@ -176,6 +169,7 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 })
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -184,20 +178,3 @@ app.listen(PORT, () => {
 function generateRandomString() {
 return Math.floor(1 + Math.random() * Number.MAX_VALUE).toString(36).substring(1, 7);
 }
-
-
-
-
-
-
-
-
-//res.locals
-
-
-
-
-
-
-
-
